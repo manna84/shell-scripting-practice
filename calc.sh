f11b74c4ea108cc8f1008f45aac9f3e3b215d93e
manna84 <60120742+manna84@users.noreply.github.com>
2020-03-25 02:39:25 -0400

    user outline added as 'selection'

--- calc.sh
+++ calc.sh
@@ -15,6 +15,9 @@ echo "Subtract"
 echo "Exit"
 
 # Then, capture the user selection.
+
+read selection
+
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
