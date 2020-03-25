5a33942c23e8f6372cf4fe198982bc8d29df0f9c
manna84 <60120742+manna84@users.noreply.github.com>
2020-03-25 03:17:36 -0400

    Redisplay menu and exit configured

--- calc.sh
+++ calc.sh
@@ -4,51 +4,56 @@
 
 echo "Hello, welcome to the calculator program"
 
-# Then, display a menu that outlines the possible operations:
-  # Add
-  # Subtract
-  # Exit
-
-echo "Please make a selection below:"
-echo "Add"
-echo "Subtract"
-echo "Exit"
-
-# Then, capture the user selection.
-
-read selection
-
-# If the selection matches a supported operation, execute the operation.
-
-case $selection in
-    "Add")
-    echo "Please provide first number"
-    read first_number
-     echo "Please provide second number"
-    read second_number
-    echo "The sum of $first_number and $second_number is:"
-    expr $first_number + $second_number
-    ;;
-
-    "Subtract")
-    echo "Please provide first number"
-    read first_number
-     echo "Please provide second number"
-    read second_number
-    echo "The difference of $first_number and $second_number is:"
-    expr $first_number - $second_number
-    ;;
-
-    "Exit")
-    echo "User selected 'Exit'"
-    ;;
-
-# If the selection does not match a support operation, display an error message.
-
-    *)
-    echo "Unsupported operation: $selection"
-    echo "Please try again"
-    ;;
-esac
-
 # When the operation is complete, redisplay the menu.
+
+while true; do
+
+  # Then, display a menu that outlines the possible operations:
+    # Add
+    # Subtract
+    # Exit
+
+  echo "Please make a selection below:"
+  echo "Add"
+  echo "Subtract"
+  echo "Exit"
+
+  # Then, capture the user selection.
+
+  read selection
+
+  # If the selection matches a supported operation, execute the operation.
+
+  case $selection in
+      "Add")
+      echo "Please provide first number"
+      read first_number
+      echo "Please provide second number"
+      read second_number
+      echo "The sum of $first_number and $second_number is:"
+      expr $first_number + $second_number
+      ;;
+
+      "Subtract")
+      echo "Please provide first number"
+      read first_number
+      echo "Please provide second number"
+      read second_number
+      echo "The difference of $first_number and $second_number is:"
+      expr $first_number - $second_number
+      ;;
+
+      "Exit")
+      echo "Please come again. Goodbye!"
+      exit 0
+      ;;
+
+  # If the selection does not match a support operation, display an error message.
+
+      *)
+      echo "Unsupported operation: $selection"
+      echo "Please try again"
+      ;;
+  esac
+done
+
